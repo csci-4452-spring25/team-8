a8316d4392766e7c7dc44b4263b7f376040ae700
kadonne <nanolike.limit@gmail.com>
2025-04-15 09:34:00 -0500

    fix

---
+++ terraform/main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "ammar_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Ammar's VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.ammar_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "Ammar's Subnnet"
+  }
+}
+
+resource "aws_internet_gateway" "aws_igw" {
+  vpc_id = aws_vpc.ammar_vpc.id
+  tags = {
+    Name = "Ammar's Gateway"
+  }
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.ammar_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.aws_igw.id
+  }
+  tags = {
+    Name = "Ammar's route table"
+  }
+}
+resource "aws_route_table_association" "public_rt_ass" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.ammar_vpc.id
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "d-key"
+  public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3WfrRXZHte3vS9rs2PyTO6GH3W2mUQHFV+TUxn3d+vdaJrCCernITBjf1wYmawqFR9uz7nlDr+k69aSFcJon+bTA3QLvkfxUIwXcXrSgYXdftPzH7PYCaFmOXk0Y4jCnR/Gy3LQcuws8gzI1amqhsVvhj/NMHGnfiOyWQiLNRne1ck9UnWV82vPdKbsjcWspsnpVSCn0BQ0affXmMSlljuNgXl4jA4wgDxyTVsYqJNXk3nsIofUcEvSkjtmYjsZIRLuEoAtcjkoFO8eC3CLbI2eZR009+yrT/O1+iPj/kzJbjRH12p9TPaPBhcf40N0bRt8D4RWPYkG+/ZlnVAKpswIDAQAB"
+}
+
+resource "aws_instance" "foo" {
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+  ami                         = "ami-005e54dee72cc1d00" # us-west-2
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.deployer.key_name
+
+  tags = {
+    Name = "AMMAR EC2"
+  }
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+}
